40c6c6367c21a17dd2aed5c963c5ce2ebfa2b3a3
Sarath <chandallasarath@gmail.com>
2026-05-20 11:44:48 -0700

    Update: anagram-groups - submission-0

--- Data Structures & Algorithms/anagram-groups/submission-0.cpp
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         std::unordered_map<string, vector<string>> strMap;
-
-        for (auto const& str: strs) {
-            int counts[26] = {};
+    
+        for (auto const& str : strs) {
+            int counts[26] = {0}; // Fixed initialization syntax
             for (auto const& c : str) {
                 counts[c - 'a']++;
             }
+            
             string key;
             for (int i = 0; i < 26; i++) {
-                key += std::to_string(counts[i])+'#';
+                key += std::to_string(counts[i]) + '#';
             }
             strMap[key].emplace_back(str);
         }
-
+        
         vector<vector<string>> result;
         result.reserve(strMap.size());
-        for (auto const &it : strMap) {
+        
+        // REMOVED 'const' here so std::move can actually transfer ownership
+        for (auto &it : strMap) { 
             result.push_back(std::move(it.second));
         }
-
-        return result;
         
+        return result;
     }
 };
