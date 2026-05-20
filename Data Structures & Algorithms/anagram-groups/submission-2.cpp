30144eaed0aba411efbb9e6fa3f27fe491cc3e9d
Sarath <chandallasarath@gmail.com>
2026-05-20 11:46:52 -0700

    Update: anagram-groups - submission-2

--- Data Structures & Algorithms/anagram-groups/submission-2.cpp
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        std::unordered_map<string, vector<string>> strMap;
-
-        for (auto const& str: strs) {
-            int counts[26] = {};
-            for (auto const& c : str) {
-                counts[c - 'a']++;
+        unordered_map<string , vector<string>>hash;
+        for(string s:strs){
+            vector<int>freq(26 , 0);
+            for(char ch:s){
+                freq[ch-'a']++;
             }
-            string key;
-            for (int i = 0; i < 26; i++) {
-                key += std::to_string(counts[i])+'#';
+            string key = "";
+            for(int i=0;i<26;i++){
+                key+='#'+freq[i];
             }
-            strMap[key].emplace_back(str);
-        }
 
-        vector<vector<string>> result;
-        result.reserve(strMap.size());
-        for (auto &it : strMap) {
-            result.push_back(std::move(it.second));
+            hash[key].push_back(s);
         }
-
-        return result;
-        
+        vector<vector<string>>ans;
+        for(auto& [s , lst]:hash){
+            ans.push_back(lst);
+        }
+        return ans;
     }
 };
